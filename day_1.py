82df6ea6d5dfdd4837a3bea92673cbb257422788
pal004 <150345252+pal004@users.noreply.github.com>
2025-09-20 11:24:48 +0530

    Add files via upload

---
+++ day_1.py
@@ -0,0 +1,16 @@
+
+import streamlit as st
+
+def run():
+    st.title("SAATHI — Your Companion AI (Part 1)")
+    st.write("Welcome to Saathi, your AI buddy from India 🇮🇳")
+
+    st.markdown("""
+    ### What SAATHI Does
+    - Listens to you   
+    - Understands your emotions   
+    - Responds in fun, mindful, or supportive ways   
+    - Check your vital levels an responds with excercise reminders, keeps track of your sleep cycle and nutrition
+
+    """)
+    st.success("Part 1 setup is done — this is your intro page!")
